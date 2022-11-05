59821105516bc5c9f5d2d82831507c2b55c52d58
SHINO1256 <115599151+SHINO1256@users.noreply.github.com>
2022-11-05 12:50:46 +0900

    3章ループチャレンジ問題
    「0から100まで表示するループ」です。
    ご確認よろしくお願いします。

---
+++ for/for.php
@@ -0,0 +1,17 @@
+<?php
+// for
+for ($i = 0; $i < 101; $i++){
+    echo $i;
+    echo "<br>";
+}
+
+echo "<br>";
+// while
+$num = 0;
+while($num < 101){
+    echo $num;
+    $num++;
+    echo "<br>";
+}
+
+?>
