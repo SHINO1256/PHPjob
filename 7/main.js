fd8372b338488f534811c297689a3023f4e587e4
SHINO1256 <115599151+SHINO1256@users.noreply.github.com>
2022-11-19 13:35:12 +0900

    7章チェックテスト(関数・クラス)
    7章チェックテスト(関数・クラス)です。

---
+++ 7/main.js
@@ -0,0 +1,35 @@
+/*問1:isEven関数を実行して、
+以下の配列から偶数だけが出力されるように実装してください。*/
+
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+function isEven(elements){
+    return elements % 2 === 0;
+}
+console.log(`${numbers.filter(isEven)}は偶数です`);
+// 2,12,18,22は偶数です
+
+
+/*問2:以下の要件を満たすように実装してください。
+【要件】
+・インスタンス化した時にガソリンとナンバーが
+セットされるようなCarクラスを作成する
+・「ガソリンは〇〇です。ナンバーは△△です」と出力される
+「getNumGas」という関数を作成する*/
+
+// Carクラスを作成
+class Car{
+    //”ガソリン”と”ナンバー”という値を受け取る
+    constructor(gas,number){
+        this.gas = gas;
+        this.number = number;
+    }
+}
+
+//Carクラスをインスタンス化
+let anyCar = new Car('〇〇', '△△');
+
+//「getNumGas」という関数の作成
+function getNumGas(){
+    console.log(`ガソリンは${anyCar.gas}です。ナンバーは${anyCar.number}です。`);
+};
+getNumGas();
