26dd7d0ef66622fc1ffdf53320f2773a583a40d6
SHINO1256 <115599151+SHINO1256@users.noreply.github.com>
2022-11-22 08:42:32 +0900

    3章チェックテスト再々提出
    3章チェックテスト(4択クイズ)に関するご指摘の点を修正しました。

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -1,15 +1,20 @@
 <?php 
-//[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+//[question.php]から送られてきた名前の変数、
 $name=$_POST["name"];
-$port=$_POST["port"];
-$language=$_POST["language"];
-$command=$_POST["command"];
-$answerPort="80";
-$answerLanguage="HTML";
-$answerCommand="select";
 
-
+/*先生にいただいたご指摘：
+「回答の変数が[question.php]から送られてきたものになっていません。」
+⇒以下（8～14行目）のように修正します*/
+//選択した回答
+$selectedPort=$_POST["port"]; //修正しました
+$selectedLanguage=$_POST["language"]; //修正しました
+$selectedCommand=$_POST["command"]; //修正しました
+//問題の答えの変数を作成
+$a_Port=$_POST["answerPort"]; //修正しました
+$a_Language=$_POST["answerLanguage"]; //修正しました
+$a_Command=$_POST["answerCommand"]; //修正しました
 ?>
+
 <html>
 <link rel="stylesheet" href="css/style.css">
 <body  style="background-color:grey;" >
@@ -17,37 +22,37 @@ $answerCommand="select";
 <br>
 <br>
 <p><?php echo $name; ?>さんの結果は・・・？</p>
+<!--先生にいただいたご指摘：「以下のような要件があるので、この処理は
+if文を3回呼び出さずに、関数で定義してから呼び出すようにします。：
+選択した回答と正解が一致していれば「正解！」、
+一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する」
+⇒以下（27～52行目）のように修正します。-->
+
+<?php
+function getTrueOrFalse($selected_, $a_){
+    if( $t_or_f = ($selected_ == $a_)){
+    print "正解！";} else{
+        print "残念・・・";
+    }  
+}
+?>
+
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-if($port == $answerPort){
-        $result = "正解！";
-    } else{
-        $result = "残念・・・";
-    }
- echo $result;
+ getTrueOrFalse($selectedPort, $a_Port);
 ?>
 
 <p>②の答え</p>  
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-if($language == $answerLanguage){
-        $result = "正解！";
-    } else{
-        $result = "残念・・・";
-    }
- echo $result;
+ getTrueOrFalse($selectedLanguage, $a_Language);
 ?>
 
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-if($command == $answerCommand){
-        $result = "正解！";
-    } else{
-        $result = "残念・・・";
-    }
- echo $result;
+ getTrueOrFalse($selectedCommand, $a_Command);
 ?>
 
 </div>
--- 3-4/question.php
+++ 3-4/question.php
@@ -9,7 +9,7 @@ $command_arr=["join","select","insert","update"];
 
 //② ①で作成した、配列から正解の選択肢の変数を作成
 //先生からのコメント：「(2)question.phpの解答の選択肢は①で作成した配列から作成するようにします。」
-$answerPort= $port_arr[0]; //修正しました
+$answerPort=$port_arr[0]; //修正しました
 $answerLanguage=$language_arr[3]; //修正しました
 $answerCommand=$command_arr[1]; //修正しました
 ?>
@@ -26,12 +26,11 @@ $answerCommand=$command_arr[1]; //修正しました
     <input type="hidden" name="name" value="<?php echo $name; ?>" />
 
     <h2>①ネットワークのポート番号は何番？</h2>
-    <?php 
-        foreach($port_arr as $value){ ?>
+    <?php foreach($port_arr as $value){ ?>
         <input type="radio" name="port" value="<?php echo $value; ?>" />
-        <?php echo $value; 
-        }   
-        ?>
+        <?php echo $value;
+    } 
+    ?>
         <input type="hidden" name="answerPort" value="<?php echo $answerPort; ?>" />
 
     <h2>②Webページを作成するための言語は？</h2>
